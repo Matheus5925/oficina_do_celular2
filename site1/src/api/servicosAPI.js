178a8c1f6b295e0c625651ad850048db996008b2
Matheus Fagundes <mfagundesfernandes0617@gmail.com>
2022-06-15 11:53:11 -0300

    8 commit

--- site1/src/api/servicosAPI.js
+++ site1/src/api/servicosAPI.js
@@ -3,18 +3,19 @@ const api = axios.create({
     baseURL: "http://localhost:5000"
 })
 
-export async function CadastarServicos(servicos) {
+export async function cadastrarServicos(nome, email, telefone, cpf, MarcaCelular, ModeloCelular, entrega, 
+    devolucao, preco, problema) {
     const resposta = await api.post('/servicos', {
-        nome: "a",
-        email: " will@gmail.com",
-        cpf: "48525648115 ",
-        MarcaCelular: " Motorola",
-        ModeloCelular: " G9 Power",
-        entrega: "2022-04-05",
-        devolucao: "2022-04-12",
-        preco: 90.5,
-        problema: "Celular não liga ",
-        telefone: " 11985451815"
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        cpf: cpf,
+        MarcaCelular: MarcaCelular,
+        ModeloCelular: ModeloCelular,
+        entrega: entrega,
+        devolucao: devolucao,
+        preco: preco,
+        problema: problema
     })
     return resposta.data;
 }
--- site1/src/pages/cadastrar/index.js
+++ site1/src/pages/cadastrar/index.js
@@ -2,11 +2,36 @@ import './index.scss';
 import '../../common/common.scss'
 import Cabecalho from '../../components/cabecalho';
 import DashbordLateral from '../../components/lateral';
+import { useEffect, useState} from 'react'
 
 
 import PortaSaida from '../../assets/image/Foto-saida.png';
+import { cadastrarServicos } from '../../api/servicosAPI';
 
 export default function Cadastrar(){
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [cpf, setCpf] = useState('');
+    const [MarcaCelular, setMarcaCelular] = useState('');
+    const [ModeloCelular, setModeloCelular] = useState('');
+    const [entrega, setEntrega] = useState(0);
+    const [devolucao, setDevolucao] = useState(0);
+    const [preco, setPreco] = useState(0);
+    const [problema, setProblema] = useState('');
+    const [erro, setErro] = useState('')
+
+    async function ClickCadastrar() {
+          try {
+                const r = await cadastrarServicos(nome, email, telefone, cpf, MarcaCelular, ModeloCelular, entrega, 
+                    devolucao, preco, problema);
+          } catch (err) {
+              if (err.response.status === 401) {
+                    setErro(err.response.data.erro)
+              }
+          }
+    }
+
     return(
         <div className="Pagina-cadastrar">
             <Cabecalho></Cabecalho>
@@ -21,13 +46,13 @@ export default function Cadastrar(){
                         <div class="entrada-formulario">
                             <div class="input-name">
                                 <label><strong>Nome:</strong></label>
-                                <input type="text" name="nome" id="nome_cliente"/>
+                                <input type="text" name="nome" id="nome_cliente" value={nome} onChange={e => setNome(e.target.value)}/>
                                 <hr  width="350" class="separa-cont-consulta"/>
                             </div>
                             <div>
                                 <div class="input-name">
                                     <label><strong>E-mail:</strong></label>
-                                    <input type="email" name="e-mail" id="email_cliente"/>
+                                    <input type="email" name="e-mail" id="email_cliente" value={email} onChange={e => setEmail(e.target.value)}/>
                                     <hr  width="350" class="separa-cont-consulta"/>
                                 </div>
                             </div>
@@ -37,7 +62,7 @@ export default function Cadastrar(){
                         <div class="entrada-formulario">
                             <div class="input-name">
                                 <label><strong>Telefone de contato:</strong></label>
-                                <input type="tel" name="tel-contato" id="tel-cliente"/>
+                                <input type="tel" name="tel-contato" id="tel-cliente" value={telefone} onChange={e => setTelefone(e.target.value)}/>
                                 <hr  width="320" class="separa-cont-consulta"/>
                             </div>
                             <div>
@@ -45,7 +70,7 @@ export default function Cadastrar(){
                                     <label><strong>CPF:</strong></label>
                                     <input type="text" name="cpf" 
                                     pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" 
-                                    title="Digite um CPF no formato: xxx.xxx.xxx-xx" maxlength="14"/>
+                                    title="Digite um CPF no formato: xxx.xxx.xxx-xx" maxlength="14" value={cpf} onChange={e => setCpf(e.target.value)}/>
                                     <hr  width="350" class="separa-cont-consulta"/>
                                 </div>
                             </div>
@@ -54,13 +79,13 @@ export default function Cadastrar(){
                         <div class="entrada-formulario">
                             <div class="input-name">
                                 <label><strong>Marca do celular:</strong></label>
-                                <input type="text" name="marca-do-celular" id="marca_celular_cliente"/>
+                                <input type="text" name="marca-do-celular" id="marca_celular_cliente" value={MarcaCelular} onChange={e => setMarcaCelular(e.target.value)}/>
                                 <hr  width="350" class="separa-cont-consulta"/>
                             </div>
                             <div>
                                 <div class="input-name">
                                     <label><strong>Modelo do celular:</strong></label>
-                                    <input type="text" name="mode_celular" id="modelo_celular_cliente"/>
+                                    <input type="text" name="mode_celular" id="modelo_celular_cliente" value={ModeloCelular} onChange={e => setModeloCelular(e.target.value)}/>
                                     <hr  width="350" class="separa-cont-consulta"/>
                                 </div>
                             </div>
@@ -69,20 +94,20 @@ export default function Cadastrar(){
                         <div class="entrada-formulario  data-preço">
                             <div class="input-name">
                                 <label><strong>Data entrega na loja:</strong></label>
-                                <input type="date" name="data-entrega-loja" id="data_entrega_loja_cliente"/>
+                                <input type="date" name="data-entrega-loja" id="data_entrega_loja_cliente" value={entrega} onChange={e => setEntrega(e.target.value)}/>
                                 <hr  width="220" class="separa-cont-consulta"/>
                             </div>
                             <div>
                                 <div class="input-name">
                                     <label><strong>Data devolução cliente:</strong></label>
-                                    <input type="date" name="data-devolu-cliente" id="data_devolu_cliente"/>
+                                    <input type="date" name="data-devolu-cliente" id="data_devolu_cliente" value={devolucao} onChange={e => setDevolucao(e.target.value)}/>
                                     <hr  width="250" class="separa-cont-consulta"/>
                                 </div>
                             </div>
                             <div>
                                 <div class="input-name">
                                     <label><strong>Preço final:</strong></label>
-                                    <input type="text"  placeholder='R$'></input>
+                                    <input type="text"  placeholder='R$' value={preco} onChange={e => setPreco(e.target.value)}></input>
                                     <hr  width="200" class="separa-cont-consulta"/>
                                 </div>
                             </div>
@@ -91,13 +116,16 @@ export default function Cadastrar(){
                         <div class="entrada-formulario">
                             <div class="input-name">
                                 <label><strong>Problema celular:</strong></label>
-                                <textarea class="problema-cllr" cols="30" rows="10"></textarea>
+                                <textarea class="problema-cllr" cols="30" rows="10" value={problema} onChange={e => setProblema(e.target.value)}></textarea>
                                 <hr  width="350" class="separa-cont-consulta"/>
                             </div>
                         </div>
 
                         <div class="botao-login">
-                            <a href=""> Cadastrar </a>
+                            <button onClick={ClickCadastrar}> Cadastrar </button>
+                        </div>
+                        <div className='msg-erro'>
+                            {erro}
                         </div>
                     </form>
                 </section>
