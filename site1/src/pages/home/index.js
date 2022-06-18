2388c42fd86c631ae6b9a2d7da431cf7613d6c80
Matheus5925 <mfagundesfernandes0617@outlook.com>
2022-06-18 00:25:20 -0300

    11 commit

--- site1/src/pages/home/index.js
+++ site1/src/pages/home/index.js
@@ -14,80 +14,80 @@ import { Link } from 'react-router-dom';
 export default function Home(){
     return(
         <div className="Pagina-home">
-            <main>
-                <section className='faixa-1'>
-                    <header className='header-fx1'>
-                        <div className='menu-home'>
-                            <img className='img-menu-home' src={ImgMenuHome} alt=''></img>
-                            <p>Menu</p>
+                <main>
+                    <section className='faixa-1'>
+                        <header className='header-fx1'>
+                            <div className='menu-home'>
+                                <img className='img-menu-home' src={ImgMenuHome} alt=''></img>
+                                <p>Menu</p>
+                            </div>
+                            
+                            <Link to='/' className='login-home'>
+                                <a>Login</a>
+                                
+                            </Link>
+                        </header>
+                        <main className='main-fx1'>
+                            <div className='ondas-fx1'>
+                            
                         </div>
+                        </main>
                         
-                        <Link to='/' className='login-home'>
-                            <a>Login</a>
+                                
+                    </section>
+                    <section className='faixa-2-produtos'>
+                            <div className='titulo-fx2'>
+                                <h1>Produtos</h1>
+                            </div>
+                            <div className='parte-produtos'>
+                                <div className='img-seta-esquer-fx2'>
+                                    <img className='img-seta-produto' src={SetaEsquerda}></img>
+                                </div>
                             
-                        </Link>
-                    </header>
-                    <main className='main-fx1'>
-                        <div className='ondas-fx1'>
-                          
-                       </div>
-                    </main>
-                       
-                            
+                                <div className='card-produtos'>
+                                    <img className='imgs-produtos' src={foneBlue} alt=''></img>
+                                    <div className='text-cards'>
+                                        <p>Fone Bluetooth</p>
+                                        <h4>R$189,90</h4>
+                                    </div>
+                                </div>
+                                <div className='card-produtos'>
+                                    <img className='imgs-produtos' src={Caixinha} alt=''></img>
+                                    <div className='text-cards'>
+                                        <p>Caixa de Som</p>
+                                        <h4>R$139,90</h4>
+                                    </div>
+                                
+                                </div>
+
+                                <div className='card-produtos'>
+                                    <img className='imgs-produtos' src={relogio} alt=''></img>
+                                    <div className='text-cards'>
+                                        <p>Relogio Cassio</p>
+                                        <h4>R$279,90</h4>
+                                    </div>
+                                </div>
+                                <div className='card-produtos'>
+                                    <img className='imgs-produtos' src={Carregador} alt=''></img>
+                                    <div className='text-cards'>
+                                        <p>Carregador portátil</p>
+                                        <h4>R$159,90</h4>
+                                    </div>
+                                </div>
+
+                                <div className='img-seta-direita-fx2'>
+                                    <img className='img-seta-produto' src={SetaDireira} alt=''></img>
+                                </div>
+                            </div>
                 </section>
-                <section className='faixa-2-produtos'>
-                        <div className='titulo-fx2'>
-                            <h1>Produtos</h1>
-                        </div>
-                        <div className='parte-produtos'>
-                            <div className='img-seta-esquer-fx2'>
-                                <img className='img-seta-produto' src={SetaEsquerda}></img>
-                            </div>
-                        
-                            <div className='card-produtos'>
-                                <img className='imgs-produtos' src={foneBlue} alt=''></img>
-                                <div className='text-cards'>
-                                    <p>Fone Bluetooth</p>
-                                    <h4>R$189,90</h4>
-                                </div>
-                            </div>
-                            <div className='card-produtos'>
-                                <img className='imgs-produtos' src={Caixinha} alt=''></img>
-                                <div className='text-cards'>
-                                    <p>Caixa de Som</p>
-                                    <h4>R$139,90</h4>
-                                </div>
-                               
-                            </div>
-
-                            <div className='card-produtos'>
-                                <img className='imgs-produtos' src={relogio} alt=''></img>
-                                <div className='text-cards'>
-                                    <p>Relogio Cassio</p>
-                                    <h4>R$279,90</h4>
-                                </div>
-                            </div>
-                            <div className='card-produtos'>
-                                <img className='imgs-produtos' src={Carregador} alt=''></img>
-                                <div className='text-cards'>
-                                    <p>Carregador portátil</p>
-                                    <h4>R$159,90</h4>
-                                </div>
-                            </div>
-
-                            <div className='img-seta-direita-fx2'>
-                                <img className='img-seta-produto' src={SetaDireira} alt=''></img>
-                            </div>
-                        </div> 
+                <section className='faixa2'>
                         <div className='textos'>
-                         <h1 className='TITULO'>QUEM NÓS SOMOS ? </h1>
-                         <p className ='p1'> A oficina do celular foi criada em 2015 por Bruno Marques Vieira  ele criou a empresa com o intuito de fazer algo que ele gostava e ainda assim resolver algo que via como um deficete no cotidiano das pessoas ao seu redor como uma manutenção de qualidade e com preços acessivel ao cliente , com um serviço honesto. </p>
-                          <p className='p2'>Então ele saiu em busca dos melhores fornecedores de algunns contatos na área e no meio dessa correria toda ainda sim fez varios cursos  complementares para fornecer o melhor atendimento possivel para seus clientes e depois de muitas falhas e acertos ele conseguiu fazer sua tão esperada loj atingindo seu proposito. </p>
-                        <p className = 'p3'>Agora seu  foco é na expansão e levar os excelentes serviços dele para outros lugares e virar um nome cada vez mais conhecido e temido no mercado. </p>
-                    </div>
-                    
-            </section>
+                            <h1 className='TITULO'> QUEM NÓS SOMOS ? </h1>
+                            <p className ='p1'> A oficina do celular foi criada em 2015 por Bruno Marques Vieira  ele criou a empresa com o intuito de fazer algo que ele gostava e ainda assim resolver algo que via como um deficete no cotidiano das pessoas ao seu redor como uma manutenção de qualidade e com preços acessivel ao cliente , com um serviço honesto. </p>
+                            <p className='p2'>Então ele saiu em busca dos melhores fornecedores de algunns contatos na área e no meio dessa correria toda ainda sim fez varios cursos  complementares para fornecer o melhor atendimento possivel para seus clientes e depois de muitas falhas e acertos ele conseguiu fazer sua tão esperada loj atingindo seu proposito. </p>
+                            <p className = 'p3'>Agora seu  foco é na expansão e levar os excelentes serviços dele para outros lugares e virar um nome cada vez mais conhecido e temido no mercado. </p>
+                        </div>
+                </section>
             </main>
         </div>
-)
-}    
+    )}
