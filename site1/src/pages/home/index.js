c5bd297aaed298c1a088c7eb8d1ab38652ebd343
maridias113 <marianaferreiradias8@gmail.com>
2022-06-20 09:04:06 -0300

    novo commit

--- site1/src/pages/home/index.js
+++ site1/src/pages/home/index.js
@@ -1,28 +1,22 @@
-// import './index.scss';
-// import '../../common/common.scss'
+ import './index.scss';
+ import '../../common/common.scss'
 
-// import ImgMenuHome from '../../assets/image/logo-menu-home.png';
-// //import ImgOndasUm from '../../assets/image/Primeiraonda-landingpage.png';
-// import foneBlue from '../../assets/image/fone-produto.png';
-// import Caixinha from '../../assets/image/caixasom-produto.png';
-// import Carregador from '../../assets/image/carregador-portatil.png';
-// import relogio from '../../assets/image/relogio-produto.png';
-// import SetaEsquerda from '../../assets/image/porta-2.png';
-// import SetaDireira from '../../assets/image/porta-1.png';
-// import iphone from '../../assets/image/iphone.png';
-// import lg from '../../assets/image/lg.png';
-// import samsung from '../../assets/image/samsung.png';
-// import mi from '../../assets/image/mi.png';
-// import conserto  from '../../assets/image/conserto.png';
-// import motorola from '../../assets/image/motorola.png'
-
-// import { Link } from 'react-router-dom';
+ import ImgMenuHome from '../../assets/image/logo-menu-home.png';
+ import ImgOndasUm from '../../assets/image/Primeiraonda-landingpage.png';
+ import foneBlue from '../../assets/image/fone-produto.png';
+ import Caixinha from '../../assets/image/caixasom-produto.png';
+ import Carregador from '../../assets/image/carregador-portatil.png';
+ import relogio from '../../assets/image/relogio-produto.png';
+ import SetaEsquerda from '../../assets/image/porta-2.png';
+import SetaDireira from '../../assets/image/porta-1.png';
+import Tecnico from '../../assets/image/tecnico.png'
+ import { Link } from 'react-router-dom';
 
 export default function Home(){
     return(
         <div className="Pagina-home">
                  <main>
-                    {/*
+                
                     <section className='faixa-1'>
                         <header className='header-fx1'>
                             <div className='menu-home'>
@@ -89,45 +83,19 @@ export default function Home(){
                             </div>
                 </section>
                 <section className='faixa2'>
+                          
                         <div className='textos'>
                             <h1 className='TITULO'> QUEM NÓS SOMOS ? </h1>
                             <p className ='p1'> A oficina do celular foi criada em 2015 por Bruno Marques Vieira  ele criou a empresa com o intuito de fazer algo que ele gostava e ainda assim resolver algo que via como um deficete no cotidiano das pessoas ao seu redor como uma manutenção de qualidade e com preços acessivel ao cliente , com um serviço honesto. </p>
                             <p className='p2'>Então ele saiu em busca dos melhores fornecedores de algunns contatos na área e no meio dessa correria toda ainda sim fez varios cursos  complementares para fornecer o melhor atendimento possivel para seus clientes e depois de muitas falhas e acertos ele conseguiu fazer sua tão esperada loj atingindo seu proposito. </p>
                             <p className = 'p3'>Agora seu  foco é na expansão e levar os excelentes serviços dele para outros lugares e virar um nome cada vez mais conhecido e temido no mercado. </p>
+                           
                         </div>
-                </section>
-
-                <footer className='rodape'> 
-
-    <div className='logotipo-rodape '>
-       <img className='logo' src={iphone} alt=' flex-direction: row;'></img> 
-   </div>
-
-    <div className='logotipo-rodape '>
-         <img className='logo' src = {lg} alt = ''></img>
-    </div>
-
-    <div className='logotipo-rodape'>
-       <img className='logo' src = {samsung} alt = ''></img>
-  </div>
-
-  <div className='logotipo-rodape'>
-    <img className='logo' src = {mi} alt = ''></img>
- </div>
-
- <div className='logotipo-rodape'>
-  <img className='logo' src = {motorola} alt = ''></img>
- </div>
-
-  <div className='conserto'>
-    <img className='boneco' src = {conserto} alt =''></img>
-  </div>
-
- 
-
-</footer>
-
-                 */}
+                        <div className='imagem'>
+                                <img  className='tecnico' src={Tecnico} alt='' ></img>    
+                        </div>
+                </section>   
             </main>
         </div>
-    )}
+    )
+}
